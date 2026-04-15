e08d5049ed95a3984042a60e4817a99c0578ae6e
Jose <jj9917384@gmail.com>
2026-04-15 11:26:23 -0400

    adding animation

--- Subway Surfers/level_components/obstacles/arizona_can/can_script.gd
+++ Subway Surfers/level_components/obstacles/arizona_can/can_script.gd
@@ -1,8 +1,10 @@
 extends Node3D
 @onready var level = $'../'
 var speed = 10 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _process(delta: float):
+	animation_player.play("rolling")
 	position.x -= speed * delta * 2
 	if position.x < -15:
 		level.spawnModule(position.x+(level.num * level.offset))
